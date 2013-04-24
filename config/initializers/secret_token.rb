f9f24bc6efea8ebb806b2334a054ab34519eb8af
dpereda <dpereda@gmail.com>
2013-04-24 12:55:53 -0400

    First Commit, rails app initalized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KittyParty::Application.config.secret_token = 'eff0ceae67280a2d531ea8bc4badad6092e9f41a488958b10d639ff75509a2182908e2d98c88bcb36e51b150358293c403d4a788c8adefb67a0960c5cfb53c23'
